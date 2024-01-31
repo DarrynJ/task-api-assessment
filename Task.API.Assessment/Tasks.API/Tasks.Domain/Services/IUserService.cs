d194f01adc92035e654ec241bae09be0d751dcb7
darrynjacobs <darrynj@agilebridge.co.za>
2024-01-31 15:23:21 +0200

    added domain boilerplate

---
+++ Task.API.Assessment/Tasks.API/Tasks.Domain/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<string> LoginUser(string username, string password);
+    }
+}
