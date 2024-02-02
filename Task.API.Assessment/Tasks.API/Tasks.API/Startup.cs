a82a2b89701ccb826debd04102227c6aa4728271
darrynjacobs <darrynj@agilebridge.co.za>
2024-02-02 15:16:06 +0200

    updated startup and program classes

---
+++ Task.API.Assessment/Tasks.API/Tasks.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Tasks.Application;
+using Tasks.Infrastructure;
+
+namespace Tasks.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
