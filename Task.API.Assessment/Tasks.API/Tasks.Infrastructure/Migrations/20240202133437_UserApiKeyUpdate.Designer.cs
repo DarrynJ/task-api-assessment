c9e529b13e592f0f1bd3c68c11ddb5a20cae526a
darrynjacobs <darrynj@agilebridge.co.za>
2024-02-02 15:54:23 +0200

    added api key field

--- Task.API.Assessment/Tasks.API/Tasks.Infrastructure/Migrations/20240202133437_UserApiKeyUpdate.Designer.cs
+++ Task.API.Assessment/Tasks.API/Tasks.Infrastructure/Migrations/20240202133437_UserApiKeyUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tasks.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using Tasks.Infrastructure.Persistence;
 namespace Tasks.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240202133437_UserApiKeyUpdate")]
+    partial class UserApiKeyUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
